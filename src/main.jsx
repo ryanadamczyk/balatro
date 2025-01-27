eee50f679565f7bea15ad97dfd6cb24adf1884bb
ryanadamczyk <ryana2k14@gmail.com>
2025-01-27 12:22:45 -0500

    Configure main.jsx router for deploymnet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom' // Changed from BrowserRouter
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
 )
